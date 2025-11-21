6671017b1bc833b2cf26ade5c8e8b5dee5f42eca
SASCHADDD <saschadanu23@gmail.com>
2025-11-21 16:21:23 +0700

    menambah variabel

--- app/src/main/java/com/example/pertemuan8/view/uicontroller/Navigasi.kt
+++ app/src/main/java/com/example/pertemuan8/view/uicontroller/Navigasi.kt
@@ -3,14 +3,19 @@ package com.example.pertemuan8.view.uicontroller
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.pertemuan8.model.DataJK.JenisK
 import com.example.pertemuan8.view.Form
 import com.example.pertemuan8.view.TampilData
+import com.example.pertemuan8.viewmodel.SiswaViewModel
 
 enum class Navigasi {
     Formulirku,
@@ -19,9 +24,12 @@ enum class Navigasi {
 
 @Composable
 fun DataApp(
+    modifier: Modifier= Modifier,
+    viewModel: SiswaViewModel = viewModel(),
     navController: NavHostController = rememberNavController()
 ){
     Scaffold { isiRuang ->
+        val uiState = viewModel.statusUI.collectAsState()
         NavHost(
             navController = navController,
             startDestination = Navigasi.Formulirku.name,
@@ -30,14 +38,18 @@ fun DataApp(
                 .padding(isiRuang)
         ) {
             composable(route = Navigasi.Formulirku.name) {
+                val konteks = LocalContext.current
                 Form (
+                    PilihanJK = JenisK.map {id -> konteks.resources.getString(id)},
                     OnsubmitBtnClick = {
+                        viewModel.setSiswa(it)
                         navController.navigate(Navigasi.Detail.name)
                     }
                 )
             }
             composable(route = Navigasi.Detail.name){
                 TampilData (
+                    statusUiSiswa = uiState.value,
                     onBackBtnClick = {
                         cancelAndBackToFormulir(navController)
                     }
